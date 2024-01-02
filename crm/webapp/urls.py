0488c28264cabe2216c0c07582ff73b2de53445f
RobertRuai <robertruai4@gmail.com>
2024-01-02 17:29:32 +0300

    view singular suspect path implemented

--- crm/webapp/urls.py
+++ crm/webapp/urls.py
@@ -13,8 +13,6 @@ urlpatterns = [
 
     path('user-logout', views.user_logout, name="user-logout"),
 
-    # CRUD
-
     path('dashboard', views.dashboard, name="dashboard"),
 
     path('create-record', views.create_record, name="create-record"),
@@ -23,9 +21,15 @@ urlpatterns = [
 
     path('record/<int:pk>', views.singular_record, name="record"),
 
-    path('delete-record/<int:pk>', views.delete_record, name="delete-record")
+    path('delete-record/<int:pk>', views.delete_record, name="delete-record"),
 
     path('reportsuspect/', views.reportsuspectView, name="reportsuspect"),
+
+    path('suspect/<int:pk>', views.singular_suspect, name="suspect"),
+
+
+#	 path('crime_reports_list/<int:pk>', views.crime_reports_list, name="crime_reports_list"),
+#    path('create-report/', views.create_report, name="create-report")
 #    path('wanted/', views.wantedView, name="wanted-list"),
 #    path('tips/', views.tipsView, name="tips"),
 #    path('announcements/', views.news, name="news")
