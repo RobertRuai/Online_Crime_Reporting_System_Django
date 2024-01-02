1a4be7f10748ba4be2c3f451137733db4b812efd
RobertRuai <robertruai4@gmail.com>
2024-01-02 17:23:50 +0300

    tried implementing a private model,but changes failed to migrate

--- crm/webapp/models.py
+++ crm/webapp/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Record(models.Model):
     creation_date = models.DateTimeField(auto_now_add=True)
@@ -19,7 +21,7 @@ class ReportSuspect(models.Model):
     genderList = (
         ('male', 'male'),
         ('female', 'female')
-     )
+    )
     suspect_name = models.CharField(max_length=30, null=True)
     location = models.CharField(max_length=30, null=True)
     dresscode = models.CharField(max_length=200, null=True)
@@ -27,20 +29,21 @@ class ReportSuspect(models.Model):
     sex = models.CharField(choices=genderList, max_length=200)
 
     def __str__(self):
-        return self.suspect_Name
+        return self.suspect_name
 
 
-#class wantedSuspect(models.Model):
-#    genderList = (
-#        ('male', 'male'),
-#        ('female', 'female')
-#    )
+#class CrimeReport(models.Model):
+#    STATUS_CHOICES = [
+#        ('pending', 'Pending'),
+#        ('solved', 'Solved'),
+#    ]
 
-#    name = models.CharField(max_length=200, null=True)
-#    age = models.CharField(max_length=200, null=True)
-#    picture = models.ImageField(max_length=200, null=True)
-#	 sex = models.CharField(choices=genderList, max_length=200)
-#    datePosted = models.DateTimeField(auto_now=True)
+#    title = models.CharField(max_length=100)
+#    suspect_name = models.CharField(max_length=100)
+#    description = models.TextField()
+#    reported_by = models.ForeignKey(User, on_delete=models.CASCADE)
+#    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+#    created_at = models.DateTimeField(auto_now_add=True)
 
-#   def __str__(self):
-#       return self.name
+#    def __str__(self):
+#        return self.title
