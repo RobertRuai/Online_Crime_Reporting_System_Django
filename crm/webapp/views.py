b40933d4b9aec73c6a214f60f052ecb457d28066
RobertRuai <robertruai4@gmail.com>
2024-01-02 17:27:55 +0300

    minor edits

--- crm/webapp/views.py
+++ crm/webapp/views.py
@@ -13,135 +13,99 @@ from django.contrib import messages
 
 
 
-# - Homepage 
-
+# - Homepage
 def home(request):
-
     return render(request, 'webapp/index.html')
 
 
 # - Register user
-
 def register(request):
-
     form = CreateUserForm()
-
     if request.method == "POST":
-
         form = CreateUserForm(request.POST)
-
         if form.is_valid():
-
             form.save()
-
             messages.success(request, "Account created successfully!")
-
             return redirect("my-login")
-
     context = {'form':form}
-
     return render(request, 'webapp/register.html', context=context)
 
 
 # - Login a user
-
 def my_login(request):
-
     form = LoginForm()
-
     if request.method == "POST":
-
         form = LoginForm(request, data=request.POST)
-
         if form.is_valid():
-
             username = request.POST.get('username')
             password = request.POST.get('password')
-
             user = authenticate(request, username=username, password=password)
-
             if user is not None:
-
                 auth.login(request, user)
-
                 return redirect("dashboard")
-
     context = {'form':form}
-
     return render(request, 'webapp/my-login.html', context=context)
 
 
 # - Dashboard
-
 @login_required(login_url='my-login')
 def dashboard(request):
-
     my_records = Record.objects.all()
-
-    context = {'records': my_records}
-
+    my_suspects = ReportSuspect.objects.all()
+    context = {
+        'records': my_records,
+        'suspects': my_suspects
+    }
     return render(request, 'webapp/dashboard.html', context=context)
 
 
 # - Create a record 
-
 @login_required(login_url='my-login')
 def create_record(request):
-
     form = CreateRecordForm()
-
     if request.method == "POST":
-
         form = CreateRecordForm(request.POST)
-
         if form.is_valid():
-
             form.save()
-
             messages.success(request, "Your record was created!")
-
             return redirect("dashboard")
-
     context = {'form': form}
-
     return render(request, 'webapp/create-record.html', context=context)
+
+	
+#@login_required(login_url='my-login')
+#def create_report(request):
+#    cform = CrimeReportForm()
+#    if request.method == "POST":
+#        cform = CrimeReportForm(request.POST)
+#    if cform.is_valid():
+#        cform.save()
+#        messages.success(request, "Your report was created!")
+#        return redirect("dashboard")
+#    context = {'cform': cform}
+#    return render(request, 'webapp/create-report.html', context=context)
 
 
 # - Update a record 
-
 @login_required(login_url='my-login')
 def update_record(request, pk):
-
     record = Record.objects.get(id=pk)
-
     form = UpdateRecordForm(instance=record)
-
     if request.method == 'POST':
-
         form = UpdateRecordForm(request.POST, instance=record)
-
         if form.is_valid():
-
             form.save()
-
             messages.success(request, "Your record was updated!")
-
             return redirect("dashboard")
-        
     context = {'form':form}
-
     return render(request, 'webapp/update-record.html', context=context)
 
 
 # - Read / View a singular record
-
 @login_required(login_url='my-login')
 def singular_record(request, pk):
-
     all_records = Record.objects.get(id=pk)
-
     context = {'record':all_records}
-
     return render(request, 'webapp/view-record.html', context=context)
 
 
@@ -149,27 +113,20 @@ def singular_record(request, pk):
 
 @login_required(login_url='my-login')
 def delete_record(request, pk):
-
     record = Record.objects.get(id=pk)
-
     record.delete()
-
     messages.success(request, "Your record was deleted!")
-
     return redirect("dashboard")
 
 
-
 # - User logout
-
 def user_logout(request):
-
     auth.logout(request)
-
     messages.success(request, "Logout success!")
-
     return redirect("my-login")
 
+
+# - report suspect
 @login_required(login_url='my-login')
 def reportsuspectView(request):
     rsForm = reportSuspectForm()
@@ -177,14 +134,23 @@ def reportsuspectView(request):
         rsForm = reportSuspectForm(request.POST)
         if rsForm.is_valid():
             rsForm.save()
-        return redirect('dashboard')
+            messages.success(request, "Suspect report successful!")
+            return redirect('dashboard')
 
     context ={'rsForm':rsForm}
     return render(request, 'webapp/reportsuspect.html', context)
 
-#def wantedView(request):
-#    wanted_list = wantedSuspect.objects.all()
-#    context = {
-#        'wanted_list': wanted_list
-#    }
-#    return render(request, 'webapp/wanted.html', context)
+
+# - view a suspect record
+@login_required(login_url='my-login')
+def singular_suspect(request, pk):
+    all_suspects = ReportSuspect.objects.get(id=pk)
+    context = {'suspect':all_suspects}
+    return render(request, 'webapp/view-suspect.html', context=context)
+
+
+# - view crime reports
+#@login_required(login_url='my-login')
+#def crime_reports_list(request, pk):
+#    crime_reports = CrimeReport.objects.get(id=pk)
+#    return render(request, 'webapp/crime_reports_list.html', {'crime_reports': crime_reports})
