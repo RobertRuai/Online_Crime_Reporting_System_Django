d425a50065c7a3d02f7c23ed34aac8078b1e944d
RobertRuai <robertruai4@gmail.com>
2024-01-02 17:19:06 +0300

    tried implementing an adminOnly crime report model,still needs some work

--- crm/webapp/admin.py
+++ crm/webapp/admin.py
@@ -3,8 +3,13 @@ from django.contrib import admin
 # Register your models here.
 
 from . models import *
-#ReportSuspect, wantedSuspect
 
+
+#class CrimeReportAdmin(admin.ModelAdmin):
+#    list_display = ('title', 'suspect_name',  'status', 'reported_by', 'created_at')
+#    list_filter = ('status', 'created_at')
+#    search_fields = ('title', 'suspect_name', 'description', 'reported_by__username')
+
+#admin.site.register(CrimeReport, CrimeReportAdmin)
 admin.site.register(Record)
 admin.site.register(ReportSuspect)
-#admin.site.register(wantedSuspect)
