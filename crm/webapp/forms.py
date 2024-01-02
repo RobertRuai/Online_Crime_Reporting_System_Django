d7cd11b7e829e4ff40f1ab0f26a0cb63716378d6
RobertRuai <robertruai4@gmail.com>
2024-01-02 17:22:11 +0300

    crime report form implemented

--- crm/webapp/forms.py
+++ crm/webapp/forms.py
@@ -8,26 +8,22 @@ from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 from django.forms.widgets import PasswordInput, TextInput
 
-# - Register/Create a user
 
+# - Register/Create a user
 class CreateUserForm(UserCreationForm):
 
     class Meta:
-
         model = User
         fields = ['username', 'password1', 'password2']
 
 
 # - Login a user
-
 class LoginForm(AuthenticationForm):
-
     username = forms.CharField(widget=TextInput())
     password = forms.CharField(widget=PasswordInput())
 
 
-# - Create a complaint
-
+# - Create a complaint/User
 class CreateRecordForm(forms.ModelForm):
 
     class Meta:
@@ -36,8 +32,7 @@ class CreateRecordForm(forms.ModelForm):
         fields = ['first_name', 'last_name', 'email', 'phone', 'address', 'city', 'province', 'country']
 
 
-# - Update a cpmplaint
-
+# - Update a cpmplaint/User
 class UpdateRecordForm(forms.ModelForm):
 
     class Meta:
@@ -45,13 +40,20 @@ class UpdateRecordForm(forms.ModelForm):
         model = Record
         fields = ['first_name', 'last_name', 'email', 'phone', 'address', 'city', 'province', 'country']
 
-#added forms
-class reportSuspectForm(ModelForm):
+
+# - report a suspect/User
+class reportSuspectForm(forms.ModelForm):
+
     class Meta:
         model = ReportSuspect
         fields = '__all__'
+        widgets = {
+            'sex': forms.Select(attrs={'class': 'form-control'}),
+        }
 
-#class reportCrimeForm(ModelForm):
+
+# - fill in a crime report/Admin
+#class CrimeReportForm(forms.ModelForm):
 #    class Meta:
-#        model = ReportCrimeModel
-#        fields ='__all__'
+#        model = CrimeReport
+#        fields = ['title', 'suspect_name', 'description', 'status']
